f1a96c2132bdafcc516861e6457191a1ab0559ed
Ardy <ardy@defi.sucks>
2023-02-07 18:37:38 -0400

    style: more stars

--- src/containers/StarsBackground/index.tsx
+++ src/containers/StarsBackground/index.tsx
@@ -15,6 +15,7 @@ export function StarsBackground() {
   return (
     <StarsContainer>
       <div id="stars"></div>
+      <div id="stars1"></div>
       <div id="stars2"></div>
       <div id="stars3"></div>
     </StarsContainer>
