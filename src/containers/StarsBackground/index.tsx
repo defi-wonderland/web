ad5a0f971867916e49aee651f6eff1512bcaa150
Ardy <ardy@defi.sucks>
2023-02-16 09:31:04 -0400

    chore: remove sass dependency

--- src/containers/StarsBackground/index.tsx
+++ src/containers/StarsBackground/index.tsx
@@ -1,23 +1,101 @@
 import styled from "styled-components";
-import "~/assets/css/stars.sass";
 
-const StarsContainer = styled.div`
+const getRandomInt = (max: number) => {
+  return Math.floor(Math.random() * max);
+};
+
+const generateStars = (stars: number) => {
+  const width = 2000;
+  const height = 4000;
+
+  let shadows = `${getRandomInt(width)}px ${getRandomInt(height)}px #fff`;
+  for (let index = 0; index < stars; index++) {
+    shadows = `${shadows}, ${getRandomInt(width)}px ${getRandomInt(
+      height
+    )}px #fff`;
+  }
+  return shadows;
+};
+
+export interface StarsContainerProps {
+  smStars: number;
+  mdStars: number;
+  lgStars: number;
+}
+
+const StarsContainer = styled.div<StarsContainerProps>`
   position: absolute;
   top: 0;
   left: 0;
 
+  & .stars {
+    width: 0.1rem;
+    height: 0.1rem;
+    box-shadow: ${(props) => generateStars(props.smStars)};
+    animation: animStar 701ms linear infinite;
+    z-index: 100;
+  }
+
+  & .stars1 {
+    width: 0.1rem;
+    height: 0.1rem;
+
+    box-shadow: ${(props) => generateStars(props.smStars)};
+    animation: animStar 701ms linear infinite;
+    z-index: 100;
+  }
+
+  & .stars2 {
+    width: 0.2rem;
+    height: 0.2rem;
+    background: transparent;
+    box-shadow: ${(props) => generateStars(props.mdStars)};
+    animation: animStar 1213ms linear infinite;
+  }
+  & .stars3 {
+    width: 0.3rem;
+    height: 0.3rem;
+    background: transparent;
+    box-shadow: ${(props) => generateStars(props.lgStars)};
+    animation: animStar2 1747ms linear infinite;
+  }
+
   & div {
     border-radius: 100%;
+  }
+
+  @keyframes animStar {
+    0% {
+      opacity: 0.1;
+    }
+    50% {
+      opacity: 0.7;
+    }
+    100% {
+      opacity: 0.1;
+    }
+  }
+
+  @keyframes animStar2 {
+    0% {
+      opacity: 0.5;
+    }
+    50% {
+      opacity: 0.1;
+    }
+    100% {
+      opacity: 0.5;
+    }
   }
 `;
 
 export function StarsBackground() {
   return (
-    <StarsContainer>
-      <div id="stars"></div>
-      <div id="stars1"></div>
-      <div id="stars2"></div>
-      <div id="stars3"></div>
+    <StarsContainer smStars={700} mdStars={700} lgStars={50}>
+      <div className="stars"></div>
+      <div className="stars1"></div>
+      <div className="stars2"></div>
+      <div className="stars3"></div>
     </StarsContainer>
   );
 }
