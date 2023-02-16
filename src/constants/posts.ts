515b4735d1e6090c934234ede0106dcfc7cf7eac
Ardy <ardy@defi.sucks>
2023-02-16 08:36:15 -0400

    fix: date

--- src/constants/posts.ts
+++ src/constants/posts.ts
@@ -3,7 +3,7 @@ export const POSTS = [
     id: "3",
     name: "Oracle Manipulation 101 (Math edition)",
     description: "What does an Uniswap v3 oracle manipulation look like?",
-    date: new Date("1/1/18").toDateString(),
+    date: new Date("12/26/22").toDateString(),
     tags: ["oracles", "Uniswap V3", "math"],
     image:
       "https://price-oracle.notion.site/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F645a9912-e67f-4cca-a108-4d9ea0dbc64c%2Fimage.png?table=block&id=e9ceba01-98dc-4cc3-84bb-7de919806a9c&spaceId=de552277-5ae2-4d88-932f-dd538421d024&width=1360&userId=&cache=v2",
