30d2b5e61ac576bdec148f45f18af5ff5ac93ede
Ardy <ardy@defi.sucks>
2023-02-20 09:50:04 -0400

    style: landing title and hero section

--- src/pages/Landing/HeroSection/index.tsx
+++ src/pages/Landing/HeroSection/index.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 import styled from "styled-components";
 
-import { Section, LiquidDistortion } from "~/components/common";
+import { Section, Distortion, MOBILE_MAX_WIDTH } from "~/components/common";
 import { useWindowDimensions } from "~/hooks/windowDimensions";
 
 const HeroDivider = styled.img`
@@ -11,19 +11,47 @@ const HeroDivider = styled.img`
   z-index: 0;
 `;
 
-const StyledHeroSection = styled(Section)``;
+const StyledHeroSection = styled(Section)`
+  & div {
+    position: absolute;
+    width: 100%;
+    top: 7rem;
+    left: 0;
+  }
+
+  @media screen and (max-width: ${MOBILE_MAX_WIDTH}) {
+    top: -2rem;
+    min-height: unset;
+    height: 50rem;
+    background-image: url("/img/hero/hero_mobile.png");
+  }
+`;
+
+export const SDistortion = styled(Distortion)``;
 
 export interface HeroSectionProps {}
 
 export const HeroSection: FC<HeroSectionProps> = ({ ...props }) => {
-  const { isMobile } = useWindowDimensions();
+  const { isMobile, isTablet } = useWindowDimensions();
   return (
-    <StyledHeroSection full backgroundImage="/img/hero/hero-bg.jpg" {...props}>
-      <LiquidDistortion
-        text="TO HELP THE WEB3 ECOSYSTEM THRIVE"
-        fontSize={isMobile ? 50 : 140}
-      />
-      <HeroDivider src="/img/hero/hero-bg-divider.png" />
-    </StyledHeroSection>
+    <>
+      <StyledHeroSection
+        full
+        backgroundImage="/img/hero/hero-bg.jpg"
+        {...props}
+      >
+        {!isMobile && (
+          <SDistortion
+            text="TO HELP THE WEB3 ECOSYSTEM THRIVE"
+            fontSize={isTablet ? 100 : 120}
+          />
+        )}
+        <HeroDivider src="/img/hero/hero-bg-divider.png" />
+      </StyledHeroSection>
+
+      {isMobile && (
+        <SDistortion text="TO HELP THE WEB3 ECOSYSTEM THRIVE" fontSize={48} />
+      )}
+    </>
   );
 };
