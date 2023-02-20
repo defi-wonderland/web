93efbd7c328e3d0b6bd0e5d0fc32dd56babb0785
Ardy <116569704+0xArdy@users.noreply.github.com>
2023-02-20 14:56:52 -0400

    fix: landing content margins (#25)
    * fix: member list on carousel
    
    * fix: center content section
    
    * fix: portfolio padding
    
    * style: fix padding on project description
    
    * fix: new star icon on landing page
    
    * fix: divider line
    
    * style: landing title and hero section
    
    * fix: optimize hero image

--- src/pages/Landing/ContentSection/ContentSection.tsx
+++ src/pages/Landing/ContentSection/ContentSection.tsx
@@ -4,6 +4,7 @@ import {
   CommunityButton,
   Cone,
   Divider,
+  DividerLine,
   DividerText,
   FirstBlockContainer,
   FirstTitle,
@@ -20,63 +21,63 @@ import { ProjectsList } from "~/pages/Portfolio/ProjectsList";
 import { RightCard } from "./RightCard";
 import { Team } from "../TeamSection";
 import StarIcon from "/img/footer/star-icon.svg";
+import MobileStar from "~/assets/lore-stars.svg";
 import Flower from "~/assets/flower.svg";
 import ConeBackground from "~/assets/landing_bg.png";
 import { Link } from "~/components/common";
 
 export function ContentSection() {
   return (
-    <>
+    <LandingContainer>
       <BackgroundContainer>
         <BG_1 type="1" align="center" />
         <Cone src={ConeBackground} alt="cone image" />
       </BackgroundContainer>
+      <Star src={StarIcon} alt="Star icon" />
+      <FirstTitle>
+        <Star src={MobileStar} alt="Star icon" />
+        developing for a greater good
+      </FirstTitle>
 
-      <LandingContainer>
-        <Star src={StarIcon} alt="Star icon" />
-        <FirstTitle>
-          <Star src={StarIcon} alt="Star icon" />
-          developing for a greater good
-        </FirstTitle>
+      <FirstBlockContainer>
+        <TextContainer>
+          <GradientText>To</GradientText>
+          <GradientText>become part</GradientText>
+          <GradientText>of</GradientText>
+          <GradientText>something bigger</GradientText>
+          <GradientText>than</GradientText>
+          <GradientText>ourselves</GradientText>
+        </TextContainer>
 
-        <FirstBlockContainer>
-          <TextContainer>
-            <GradientText>To</GradientText>
-            <GradientText>become part</GradientText>
-            <GradientText>of</GradientText>
-            <GradientText>something bigger</GradientText>
-            <GradientText>than</GradientText>
-            <GradientText>ourselves</GradientText>
-          </TextContainer>
+        <RightCard />
+      </FirstBlockContainer>
 
-          <RightCard />
-        </FirstBlockContainer>
+      <Divider>
+        <DividerLine />
+        <DividerText>WONDERLAND IN PROGRESS...</DividerText>
+        <DividerLine />
+      </Divider>
 
-        <Divider>
-          <DividerText>WONDERLAND IN PROGRESS...</DividerText>
-        </Divider>
+      <SecondBlockContainer>
+        <ProjectsList />
 
-        <SecondBlockContainer>
-          <ProjectsList />
+        <SButton>
+          <Link to="/portfolio">FULL PORTFOLIO</Link>
+        </SButton>
 
-          <SButton>
-            <Link to="/portfolio">FULL PORTFOLIO</Link>
-          </SButton>
+        <Team />
+      </SecondBlockContainer>
 
-          <Team />
-        </SecondBlockContainer>
-
-        <Icon src={Flower} alt="flower icon" />
-        <SecondTitle>
-          <div>
-            <Icon src={Flower} alt="flower icon" />
-            fix-to-earn
-          </div>
-          <CommunityButton>
-            <Link to="/team">VIEW ALL COMMUNITY</Link>
-          </CommunityButton>
-        </SecondTitle>
-      </LandingContainer>
-    </>
+      <Icon src={Flower} alt="flower icon" />
+      <SecondTitle>
+        <div>
+          <Icon src={Flower} alt="flower icon" />
+          fix-to-earn
+        </div>
+        <CommunityButton>
+          <Link to="/team">VIEW ALL COMMUNITY</Link>
+        </CommunityButton>
+      </SecondTitle>
+    </LandingContainer>
   );
 }
