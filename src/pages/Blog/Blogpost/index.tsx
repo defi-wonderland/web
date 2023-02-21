05cf6fcc8064a7ebf0f4e3dff796ba4fae35f6e5
Ardy <ardy@defi.sucks>
2023-02-21 11:13:51 -0400

    feat: append date in blogpost and fix italic font

--- src/pages/Blog/Blogpost/index.tsx
+++ src/pages/Blog/Blogpost/index.tsx
@@ -5,10 +5,14 @@ import rehypeKatex from "rehype-katex";
 import styled from "styled-components";
 import ReactMarkdown from "react-markdown";
 
+import { POSTS } from "~/constants/posts";
+
 const Background = styled.div`
-  width: 100%;
-  height: 100%;
-  padding-top: 20rem;
+  padding: 1rem 5rem;
+  border-radius: 2rem;
+  margin-top: 18rem;
+  background-color: rgba(10, 13, 23, 1);
+  z-index: 1;
 `;
 
 const Content = styled.div`
@@ -18,18 +22,20 @@ const Content = styled.div`
   color: rgba(255, 255, 255, 0.87) !important;
   padding: 1.6rem 3.2rem;
   font-size: 1.9rem;
-  font-family: "Inter", sans-serif;
+  font-family: Inter, sans-serif;
 
   h1 {
-    font-size: 4rem;
+    font-family: Inter, sans-serif !important;
     padding: 1rem 0;
-    border-bottom: 1px solid rgba(255, 255, 255, 0.3);
+    font-size: 3.6rem;
+    margin-bottom: 1rem;
   }
 
   p,
   strong,
   span,
   li {
+    font-size: 1.4rem;
     padding: 0.8rem 0;
     font-weight: 100;
   }
@@ -49,8 +55,20 @@ const Content = styled.div`
 
   a {
     color: rgba(255, 255, 255, 0.87);
-    font-weight: 500;
-    text-decoration: underline;
+    font-weight: 300;
+    border-bottom: 1px solid rgba(255, 255, 255, 0.87);
+  }
+
+  blockquote {
+    margin: 1rem 0;
+    border-left: 2px solid rgba(255, 255, 255, 0.87);
+    padding-left: 1rem;
+    font-style: italic;
+    font-family: Inter-italic;
+  }
+
+  & .date {
+    background-color: unset;
   }
 `;
 
@@ -65,6 +83,22 @@ export function Blogpost() {
         setBlog(data);
       });
   }, []);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+    const date = document.getElementsByClassName("date");
+    if (blog && !date[0]) {
+      const blogNumber = location.pathname.split("/");
+      const result = POSTS.filter((post) => post.id === blogNumber[2]);
+      const title = document.getElementsByTagName("h1");
+
+      const date = document.createElement("p");
+      date.classList.add("date");
+      date.innerHTML = result[0].date;
+
+      title[0].append(date);
+    }
+  }, [blog]);
 
   return (
     <>
