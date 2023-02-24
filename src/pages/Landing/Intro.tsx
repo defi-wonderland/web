7d461c3b0560708585afd7ad5f5e37defff80c0d
Ardy <ardy@defi.sucks>
2023-02-24 14:39:40 -0400

    feat: merge intro with landing page

---
+++ src/pages/Landing/Intro.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+import { Footer, Navbar, StarsBackground } from "~/containers";
+import { Landing } from "./Landing";
+import { Intro } from "./IntroMask/Intro";
+import { MOBILE_MAX_WIDTH } from "~/components/common";
+
+export interface StyledContainerProps {
+  showBackground: boolean;
+}
+
+const LandingContainer = styled.div<StyledContainerProps>`
+  height: ${({ showBackground }) => (showBackground ? "100%" : "98vh")};
+  overflow: hidden;
+  z-index: 2;
+
+  @media screen and (max-width: ${MOBILE_MAX_WIDTH}) {
+    height: ${({ showBackground }) => (showBackground ? "100%" : "98vh")};
+  }
+`;
+
+export function IntroductionPage() {
+  const [showBackground, setShowBackground] = useState(false);
+  return (
+    <>
+      <StarsBackground zIndex={showBackground ? 0 : 10} />
+      {showBackground && <Navbar />}
+      <LandingContainer showBackground={showBackground}>
+        <Landing />
+      </LandingContainer>
+      <Intro
+        showBackground={showBackground}
+        setShowBackground={setShowBackground}
+      />
+      {showBackground && <Footer />}
+    </>
+  );
+}
---
+++ src/pages/Landing/IntroMask/Intro.tsx
@@ -0,0 +1,77 @@
+import { useEffect, useRef, useState } from "react";
+import Draggable from "react-draggable";
+
+import { StyledNavbar } from "~/containers/Navbar/Navbar.styles";
+import {
+  DottedLine,
+  IntroContainer,
+  Key,
+  KeyBox,
+  KeyContainer,
+  Keyhole,
+  Logo,
+  Mask,
+  Mask2,
+  Text,
+} from "./Intro.styles";
+import LogoImage from "~/assets/Logo.svg";
+import VLINE from "~/assets/dotted_line.svg";
+import INTROKEY from "~/assets/intro_key.svg";
+
+interface IntroProps {
+  showBackground: boolean;
+  setShowBackground: (value: boolean) => void;
+}
+
+export function Intro({ showBackground, setShowBackground }: IntroProps) {
+  const [activateDragEffect, setDragEffect] = useState(false);
+  const [backgroundEffect, setBackgroundEffect] = useState(0);
+  const nodeRef = useRef(null);
+
+  useEffect(() => {
+    if (activateDragEffect) {
+      setShowBackground(true);
+    }
+  }, [activateDragEffect]);
+
+  return (
+    <IntroContainer className={showBackground ? "hide-intro" : ""}>
+      <StyledNavbar>
+        <Logo src={LogoImage} alt="Wonderland logo" />
+      </StyledNavbar>
+      <KeyContainer>
+        <Mask backgroundEffect={backgroundEffect} />
+        <Keyhole backgroundEffect={backgroundEffect} />
+        <Mask2 backgroundEffect={backgroundEffect} />
+
+        <DottedLine
+          backgroundEffect={backgroundEffect}
+          src={VLINE}
+          alt="dotted line"
+        />
+        <Draggable
+          axis="y"
+          bounds={{ bottom: 0, top: -350 }}
+          nodeRef={nodeRef}
+          onStop={(event, node) => {
+            if (node.lastY < -50) {
+              setTimeout(() => {
+                setDragEffect(true);
+              }, 100);
+            }
+          }}
+          onDrag={(event, node) => {
+            setBackgroundEffect(-node.y / 350);
+          }}
+        >
+          <KeyBox ref={nodeRef} backgroundEffect={backgroundEffect}>
+            <Key ref={nodeRef} src={INTROKEY} alt="Key icon" />
+          </KeyBox>
+        </Draggable>
+        <Text backgroundEffect={backgroundEffect}>
+          Drag to discover Wonderland
+        </Text>
+      </KeyContainer>
+    </IntroContainer>
+  );
+}
