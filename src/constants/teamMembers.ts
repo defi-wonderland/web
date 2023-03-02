24b3d06b04734457acd96213759f0ab3ef068cd1
Ardy <116569704+0xArdy@users.noreply.github.com>
2023-03-02 17:55:23 -0400

    feat: better member animation and other fixes (#34)
    * fix: title font color
    
    * fix: backface visibility
    
    * fix: deploy
    
    * fix: hide reverse icons
    
    * feat: update members data
    
    * fix: members border
    
    * style: set user-select none to every image
    
    * test amplifiy
    
    * test: pr preview
    
    * feat: hide description when the user clicks again
    
    * style: add member description
    
    * fix: remove dangerous code
    
    * fix: back card padding
    
    * fix: undo external link
    
    * style: add telegram icons
    
    ---------
    
    Co-authored-by: turtlemoji <78362532+turtlemoji@users.noreply.github.com>

--- src/constants/teamMembers.ts
+++ src/constants/teamMembers.ts
@@ -1,50 +1,171 @@
 export const MEMBERS = [
-  { name: "MATI", position: "Position", twitterHandle: "@test", link: "/" },
-  { name: "BILLI", position: "Position", twitterHandle: "@test", link: "/" },
-  { name: "PEPO", position: "Position", twitterHandle: "@test", link: "/" },
+  {
+    name: "MATI",
+    position: "Position",
+    twitter: "https://twitter.com/MatiasNisenson",
+    github: "",
+    description:
+      "- 10+ years in Software Development\n- Addicted to scalability and conventions\n- University dropout\n- Spent 3 years in the military\n\nFun fact: His OCD forces him to make the dishwasher look like a Tetris game!",
+  },
+  {
+    name: "BILLI",
+    position: "Position",
+    twitter: "",
+    github: "https://github.com/Billy-103",
+    description:
+      "- 10+ years in Software Development\n- Addicted to scalability and conventions\n- University dropout\n- Spent 3 years in the military\n\nFun fact: His OCD forces him to make the dishwasher look like a Tetris game!",
+  },
+  {
+    name: "PEPO",
+    position: "Position",
+    twitter: "https://twitter.com/0xPEPO",
+    github: "https://github.com/vPEPO",
+    description:
+      "- 10+ years in Software Development\n- Addicted to scalability and conventions\n- University dropout\n- Spent 3 years in the military\n\nFun fact: His OCD forces him to make the dishwasher look like a Tetris game!",
+  },
+  {
+    name: "AUSTRIAN",
+    position: "Position",
+    twitter: "https://twitter.com/0xAustrian",
+    github: "https://github.com/0xAustrian",
+    description:
+      "- 10+ years in Software Development\n- Addicted to scalability and conventions\n- University dropout\n- Spent 3 years in the military\n\nFun fact: His OCD forces him to make the dishwasher look like a Tetris game!",
+  },
   {
     name: "DR GORILLA",
     position: "Position",
-    twitterHandle: "@test",
-    link: "/",
+    twitter: "https://twitter.com/DrGorilla_md",
+    github: "https://github.com/drgorillamd",
+    description:
+      "- 10+ years in Software Development\n- Addicted to scalability and conventions\n- University dropout\n- Spent 3 years in the military\n\nFun fact: His OCD forces him to make the dishwasher look like a Tetris game!",
   },
   {
     name: "JABBERWOCK",
     position: "Position",
-    twitterHandle: "@test",
-    link: "/",
+    twitter: "https://twitter.com/0xGorillaDev",
+    github: "https://github.com/0xJabberwock",
+    description:
+      "- 10+ years in Software Development\n- Addicted to scalability and conventions\n- University dropout\n- Spent 3 years in the military\n\nFun fact: His OCD forces him to make the dishwasher look like a Tetris game!",
   },
-  { name: "NG", position: "Position", twitterHandle: "@test", link: "/" },
+  {
+    name: "NG",
+    position: "Position",
+    twitter: "https://twitter.com/ngndev",
+    github: "https://github.com/0xng",
+    description:
+      "- 10+ years in Software Development\n- Addicted to scalability and conventions\n- University dropout\n- Spent 3 years in the military\n\nFun fact: His OCD forces him to make the dishwasher look like a Tetris game!",
+  },
   {
     name: "OXGORILLA",
     position: "Position",
-    twitterHandle: "@test",
-    link: "/",
+    twitter: "https://twitter.com/0xGorillaDev",
+    github: "https://github.com/0xGorilla",
+    telegram: "https://t.me/the0xGorilla",
+    description:
+      "- 10+ years in Software Development\n- Addicted to scalability and conventions\n- University dropout\n- Spent 3 years in the military\n\nFun fact: His OCD forces him to make the dishwasher look like a Tetris game!",
   },
-  { name: "ASHITAKA", position: "Position", twitterHandle: "@test", link: "/" },
-  { name: "GAS1CENT", position: "Position", twitterHandle: "@test", link: "/" },
-  { name: "GOTZEN", position: "Position", twitterHandle: "@test", link: "/" },
+  {
+    name: "ASHITAKA",
+    position: "Position",
+    twitter: "https://twitter.com/0xAshitakah",
+    github: "https://github.com/ashitakah",
+    description:
+      "- 10+ years in Software Development\n- Addicted to scalability and conventions\n- University dropout\n- Spent 3 years in the military\n\nFun fact: His OCD forces him to make the dishwasher look like a Tetris game!",
+  },
+  {
+    name: "GAS1CENT",
+    position: "Position",
+    twitter: "https://twitter.com/gas1cent",
+    github: "https://github.com/gas1cent",
+    description:
+      "- 10+ years in Software Development\n- Addicted to scalability and conventions\n- University dropout\n- Spent 3 years in the military\n\nFun fact: His OCD forces him to make the dishwasher look like a Tetris game!",
+  },
+  {
+    name: "GOTZEN",
+    position: "Position",
+    twitter: "https://twitter.com/0xGotzen",
+    github: "https://github.com/gotzenx",
+    description:
+      "- 10+ years in Software Development\n- Addicted to scalability and conventions\n- University dropout\n- Spent 3 years in the military\n\nFun fact: His OCD forces him to make the dishwasher look like a Tetris game!",
+  },
   {
     name: "WEI3ERHASE",
     position: "Position",
-    twitterHandle: "@test",
-    link: "/",
+    twitter: "https://twitter.com/wei3erHase",
+    github: "https://github.com/wei3erHase",
+    description:
+      "- 10+ years in Software Development\n- Addicted to scalability and conventions\n- University dropout\n- Spent 3 years in the military\n\nFun fact: His OCD forces him to make the dishwasher look like a Tetris game!",
   },
-  { name: "PARTICLE", position: "Position", twitterHandle: "@test", link: "/" },
+  {
+    name: "PARTI",
+    position: "Position",
+    twitter: "https://twitter.com/0xParticle",
+    github: "https://github.com/0xParticle",
+    description:
+      "- 10+ years in Software Development\n- Addicted to scalability and conventions\n- University dropout\n- Spent 3 years in the military\n\nFun fact: His OCD forces him to make the dishwasher look like a Tetris game!",
+  },
   {
     name: "SKELETOR",
     position: "Position",
-    twitterHandle: "@test",
-    link: "/",
+    twitter: "https://twitter.com/Skeletor_Space",
+    github: "https://github.com/skeletor-spaceman",
+    description:
+      "- 10+ years in Software Development\n- Addicted to scalability and conventions\n- University dropout\n- Spent 3 years in the military\n\nFun fact: His OCD forces him to make the dishwasher look like a Tetris game!",
   },
-  { name: "SHAITO", position: "Position", twitterHandle: "@test", link: "/" },
+  {
+    name: "SHAITO",
+    position: "Position",
+    twitter: "https://twitter.com/0xShaito",
+    github: "https://github.com/0xShaito",
+    description:
+      "- 10+ years in Software Development\n- Addicted to scalability and conventions\n- University dropout\n- Spent 3 years in the military\n\nFun fact: His OCD forces him to make the dishwasher look like a Tetris game!",
+  },
   {
     name: "TURTLEMOJI",
     position: "Position",
-    twitterHandle: "@test",
-    link: "/",
+    twitter: "",
+    github: "https://github.com/turtlemoji",
+    description:
+      "- 10+ years in Software Development\n- Addicted to scalability and conventions\n- University dropout\n- Spent 3 years in the military\n\nFun fact: His OCD forces him to make the dishwasher look like a Tetris game!",
   },
-  { name: "ARDY", position: "Position", twitterHandle: "@0xArdy", link: "/" },
-  { name: "RBP", position: "Position", twitterHandle: "@test", link: "/" },
-  { name: "JOXES", position: "Position", twitterHandle: "@test", link: "/" },
+  {
+    name: "ARDY",
+    position: "Position",
+    twitter: "https://twitter.com/0xArdy",
+    github: "https://github.com/0xArdy",
+    description:
+      "- 10+ years in Software Development\n- Addicted to scalability and conventions\n- University dropout\n- Spent 3 years in the military\n\nFun fact: His OCD forces him to make the dishwasher look like a Tetris game!",
+  },
+  {
+    name: "RBP",
+    position: "Position",
+    twitter: "",
+    github: "",
+    description:
+      "- 10+ years in Software Development\n- Addicted to scalability and conventions\n- University dropout\n- Spent 3 years in the military\n\nFun fact: His OCD forces him to make the dishwasher look like a Tetris game!",
+  },
+  {
+    name: "JOXES",
+    position: "Position",
+    twitter: "https://twitter.com/0xJoxes",
+    github: "https://github.com/Joxess",
+    description:
+      "- 10+ years in Software Development\n- Addicted to scalability and conventions\n- University dropout\n- Spent 3 years in the military\n\nFun fact: His OCD forces him to make the dishwasher look like a Tetris game!",
+  },
+  {
+    name: "RACCON",
+    position: "Position",
+    twitter: "https://twitter.com/0xRaccoonDev",
+    github: "https://github.com/0xRaccoon",
+    description:
+      "- 10+ years in Software Development\n- Addicted to scalability and conventions\n- University dropout\n- Spent 3 years in the military\n\nFun fact: His OCD forces him to make the dishwasher look like a Tetris game!",
+  },
+  {
+    name: "MONO",
+    position: "Position",
+    twitter: "https://twitter.com/0x_mono",
+    github: "",
+    description:
+      "- 10+ years in Software Development\n- Addicted to scalability and conventions\n- University dropout\n- Spent 3 years in the military\n\nFun fact: His OCD forces him to make the dishwasher look like a Tetris game!",
+  },
 ];
