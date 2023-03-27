cc4aa23ea4cba7777792cfce594639b7fe48d4e2
Ardy <ardy@defi.sucks>
2023-03-27 12:11:50 -0400

    chore: change landing title

--- src/pages/Landing/HeroSection/index.tsx
+++ src/pages/Landing/HeroSection/index.tsx
@@ -1,8 +1,10 @@
 import { FC } from 'react';
 import styled from 'styled-components';
 
-import { Section, Distortion, MOBILE_MAX_WIDTH, MainTitle } from '~/components/common';
+import { Section, MOBILE_MAX_WIDTH, MainTitle } from '~/components/common';
 import { useWindowDimensions } from '~/hooks/windowDimensions';
+import VIDEO_CHROME from '~/assets/videos/video_chrome.webm';
+import VIDEO_SAFARI from '~/assets/videos/video_safari.mp4';
 
 const HeroDivider = styled.img`
   position: absolute;
@@ -33,14 +35,31 @@ const StyledHeroSection = styled(Section)`
   }
 `;
 
-export const SDistortion = styled(Distortion)``;
+export const TitleContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  margin-top: 8rem;
+
+  & video {
+    width: 90rem;
+    height: 100%;
+  }
+`;
 
 export const HeroSection: FC = ({ ...props }) => {
   const { isMobile, isTablet } = useWindowDimensions();
   return (
     <>
       <StyledHeroSection full backgroundImage='/img/hero/hero-bg.jpg' {...props}>
-        {!isMobile && <Distortion text='TO HELP THE WEB3 ECOSYSTEM THRIVE' fontSize={isTablet ? 100 : 120} />}
+        {!isMobile && (
+          <TitleContainer>
+            <video autoPlay loop muted playsInline>
+              <source src={VIDEO_SAFARI} type='video/mp4; codecs="hvc1"' />
+              <source src={VIDEO_CHROME} type='video/webm' />
+            </video>
+          </TitleContainer>
+        )}
         {isMobile && (
           <div>
             <MainTitle fontSize={4.4}>TO HELP THE WEB3 ECOSYSTEM THRIVE</MainTitle>
