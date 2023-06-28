873ac54fb2a8fc7d7b75968a3484746af03685ea
Ardy <ardy@defi.sucks>
2023-06-28 16:32:11 -0400

    chore: remove title on team page

--- src/pages/Team/index.tsx
+++ src/pages/Team/index.tsx
@@ -1,24 +1,11 @@
 import { WonderTeamSection } from './TeamGrid.tsx';
-import { BackgroundImg, Container, HeroDivider, TeamBall } from './Team.styles';
-import VIDEO_CHROME from '~/assets/videos/video_chrome.webm';
-import VIDEO_SAFARI from '~/assets/videos/video_safari.mp4';
-import { TitleContainer } from '../Landing/HeroSection';
+import { BackgroundImg, Container } from './Team.styles';
 
 export function Team() {
   return (
     <>
       <BackgroundImg type='3' align='center' />
       <Container>
-        <HeroDivider>
-          <TitleContainer>
-            <video autoPlay loop muted playsInline>
-              <source src={VIDEO_CHROME} type='video/webm' />
-              <source src={VIDEO_SAFARI} type='video/mp4; codecs="hvc1"' />
-            </video>
-          </TitleContainer>
-
-          <TeamBall />
-        </HeroDivider>
         <WonderTeamSection />
       </Container>
     </>
