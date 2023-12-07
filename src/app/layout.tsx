7452c17e62890e38bb35d0c810fa1ff42dde8581
Ardy <ardy@defi.sucks>
2023-12-07 15:03:57 -0400

    style: update favicon file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,12 @@ import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: 'Wonderland',
-
   description:
     'Wonderland is the largest core development group in Web3. We partner up with the best protocols in the world, including, among others: Optimism, Connext, and Reflexer.',
   keywords: 'Wonderland, Defi, decentralized finance, activist fund, ethereum, solidity, devs',
   robots: 'index, follow',
   openGraph: {
+    images: './share.jpg',
     url: 'https://defi.sucks',
     type: 'website',
     title: 'Wonderland',
@@ -20,9 +20,6 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <head>
-        <link rel='icon' type='image/svg+xml' href='/favicon.svg' />
-      </head>
       <body>{children}</body>
     </html>
   );
