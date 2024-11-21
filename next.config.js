595fb7fba24b79faefdffc361363b85a74a15753
0xardy <ardy@defi.sucks>
2024-11-21 13:39:51 -0400

    chore: update join page redirect

--- next.config.js
+++ next.config.js
@@ -5,6 +5,15 @@ const nextConfig = {
   compiler: {
     styledComponents: true,
   },
+  async redirects() {
+    return [
+      {
+        source: '/join',
+        destination: 'https://apply.defi.sucks/',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default withVideos(nextConfig);
