9248eb2b13fcb53fc0f97c35df3a4573158a8541
th0rOdinson <144699868+th0rOdinson@users.noreply.github.com>
2025-09-16 15:33:38 -0300

    fix: resize favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,6 +20,10 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
+      <head>
+        <link rel='icon' href='/favicon.svg' type='image/svg+xml' />
+        <link rel='icon' href='/favicon.ico' sizes='any' />
+      </head>
       <body>{children}</body>
     </html>
   );
