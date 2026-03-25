b7589a0b73b1bae3f38fcb1ddf5fcb1d3fa301e9
0xardy <ardy@wonderland.xyz>
2026-03-25 18:57:10 -0300

    feat: replace hero title with wonderland logo and subtitle

--- src/pages/landing/HeroSection.tsx
+++ src/pages/landing/HeroSection.tsx
@@ -1,23 +1,17 @@
 import styled from 'styled-components';
 
-import { Section, MOBILE_MAX_WIDTH, SquigglyTitle } from '~/components';
+import { Section, MOBILE_MAX_WIDTH, FONT_MEDIUM_L } from '~/components';
 
 const HeroSection = ({ ...props }) => {
   return (
     <>
       <StyledHeroSection full backgroundImage='/img/hero/hero-bg.jpg' {...props}>
         <TitleContainer>
-          <SquigglyTitle
-            text={`THE LEADING CORE DEVELOPMENT\nGROUP IN WEB3`}
-            sizes={{
-              lg: '14rem',
-              md: '11.1rem',
-              sm: '4rem',
-              lgvw: '10vw',
-              mdvw: '11vw',
-              smvw: '13vw',
-            }}
-          />
+          <HeroLogo src='/img/wonder-logo-text.svg' alt='Wonderland' />
+          <HeroSubtitle>
+            Foundational engineering
+            <MobileBr /> for frontier technologies
+          </HeroSubtitle>
         </TitleContainer>
 
         <HeroDivider src='/img/hero/hero-bg-divider.png' />
@@ -49,8 +43,10 @@ const StyledHeroSection = styled(Section)`
 export const TitleContainer = styled.div`
   width: 100%;
   display: flex;
+  flex-direction: column;
+  align-items: center;
   justify-content: center;
-  margin-top: 8rem;
+  margin-top: 10rem;
 
   position: absolute;
   width: 100%;
@@ -60,5 +56,38 @@ export const TitleContainer = styled.div`
   @media screen and (max-width: ${MOBILE_MAX_WIDTH}) {
     margin-top: 4rem;
     top: 10rem;
+  }
+`;
+
+const HeroLogo = styled.img`
+  width: 70rem;
+  max-width: 80%;
+  height: auto;
+
+  @media screen and (max-width: ${MOBILE_MAX_WIDTH}) {
+    width: 30rem;
+  }
+`;
+
+const MobileBr = styled.br`
+  display: none;
+
+  @media screen and (max-width: ${MOBILE_MAX_WIDTH}) {
+    display: block;
+  }
+`;
+
+const HeroSubtitle = styled.p`
+  font-family: ${FONT_MEDIUM_L};
+  font-size: 2.4rem;
+  color: white;
+  margin-top: 3rem;
+  text-align: center;
+  letter-spacing: 0.05em;
+
+  @media screen and (max-width: ${MOBILE_MAX_WIDTH}) {
+    font-size: 1.6rem;
+    margin-top: 2rem;
+    padding: 0 2rem;
   }
 `;
